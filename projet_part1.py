dace8e8ada048b72c135cc211fd24a868fdf64fa
Robin <33178913+robin-libert@users.noreply.github.com>
2019-04-17 17:38:00 +0200

    Add files via upload

---
+++ projet_part1.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 17 13:28:31 2019
+
+@author: USER
+"""
+from source_coding import *
+
+lower_case_letters = list('abcdefghijklmnopqrstuvwxyz')
+numbers = list('0123456789')
+additional_characters = list('.,;:\'?!-=/& ')
+
+def import_text(file):
+    with open('text.csv','r') as csvFile:
+        char = list()
+        for line in csvFile:
+            for symbol in line:
+                char.append(symbol.lower())
+        return char
+    
+text = import_text('text.csv')
+pDistrib = marginal_pDistrib(text)
+huffman_code = binary_huffman_code(pDistrib)
+
+encoded_text = encode_text(text)
+
+#pDistrib = {'a': 0.49,'b': 0.26 ,'c':0.12,'d':0.04,'e':0.04,'f':0.03, 'g':0.02}
+
+
+
+
+
+print('2.   marginal probability distribution : ', pDistrib, '\n')
+print('3.   Coding table : ', huffman_code, '\n')
+print('4.   Coded text : ', encoded_text)
+print('     Length of th coded text :', len(encoded_text), '\n')
+print('5.   Expected average length :', expected_average_length(pDistrib))
+print('     Empirical average length :', len(encoded_text) / len(text), '\n')
---
+++ source_coding.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 17 16:43:20 2019
+
+@author: USER
+"""
+
+def marginal_pDistrib(text):
+    text_length = len(text)
+    dictionary = dict()
+    for s in text:
+        if s not in dictionary:
+            dictionary[s] = 1
+        else:
+            dictionary[s] += 1
+    for s in dictionary:
+        dictionary[s] = dictionary[s] / text_length
+    return dictionary
+
+def binary_huffman_code(pDistrib):
+    copy = pDistrib.copy()
+    coded = dict()
+    #create new dictionary with symbols
+    for v in copy:
+        coded[v] = ""
+    summ = 0
+    while(summ != 1):
+        #find the 2 lowest valued symbols
+        min1Key = min(copy, key = copy.get)
+        min1V = min(copy.values())
+        copy.pop(min1Key)
+        min2Key = min(copy, key = copy.get)
+        min2V = min(copy.values())
+        copy.pop(min2Key)
+        summ = min1V + min2V
+        copy[min1Key+min2Key] = summ
+        for e in min1Key:
+            coded[e] += '1'
+        for e in min2Key:
+            coded[e] += '0'
+    for e in coded:
+        #reverse a string
+        coded[e] = coded[e][::-1]
+    return coded
+
+def encode_text(text):
+    distrib = marginal_pDistrib(text)
+    huffman_code = binary_huffman_code(distrib)
+    encoded_text = ""
+    for s in text:
+        encoded_text += huffman_code[s]
+    return encoded_text
+
+def expected_average_length(pDistrib):
+    huffman_code = binary_huffman_code(pDistrib)
+    n = 0
+    for s in huffman_code:
+        n += len(huffman_code[s]) * pDistrib[s]
+    return n
